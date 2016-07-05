ef62a0a8f70cbf8f75b349745f5ae38b62e5902d
Jackelyn Kjerstie <jackelynkjerstie@gmail.com>
2016-07-05 14:31:49 -0400

    Began laying out game screen
    start button visually created, score displayed, ball displayed.

--- js/game.js
+++ js/game.js
@@ -41,6 +41,7 @@ var mouseObj = {};
 gameCanvas.width = W;
 gameCanvas.height = H;
 
+//Step Two..jkh..Clear page canvas by covering it with black
 function paintCanvas() {
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, W, H);
@@ -54,3 +55,71 @@ function trackPosition(evt) {
     console.log("cursor x is:" + mouseObj.x + "cursor y is:" + mouseObj.y);
 }
 gameCanvas.addEventListener("mousemove", trackPosition, true);
+
+//Step Three..jkh..place a ball on the canvas
+var ball={}; //ball object -- curly brackets
+ball = {
+    x: 50,
+    y: 50,
+    r: 15,
+    c: "#ffffff",
+    vx: 4, //velocity x
+    vy: 8, //velocity y
+    
+    draw: function(){
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.fill();
+    }
+}
+ball.draw(); //calls the ball function creates the ball
+
+//Step Four..jkh..Place a start button on the canvas
+var startBtn ={}; //start button object
+startBtn = {
+    w: 100,
+    h: 50,
+    x: W / 2 - 50,
+    y: H / 2 - 25,
+    
+    draw: function(){
+        ctx.strokeStyle = "#FFFFFF";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        
+        ctx.font = "18px Arial, sans-serif";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText("Start", W / 2, H / 2);
+    }
+}
+startBtn.draw();
+
+//Step Five..jkh..placing score and points on canvas
+var points = 0; // game points
+function paintScore(){
+    ctx.fillStyle = "#ffffff";
+    ctx.font = "18px Arial, sans-serif";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Score: " + points, 20, 20);
+    
+}
+paintScore();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
