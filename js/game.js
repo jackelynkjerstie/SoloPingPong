051cadf646c593f49ec7116438239606a0d9ae04
Jackelyn Kjerstie <jackelynkjerstie@gmail.com>
2016-07-12 14:59:45 -0400

    game play movements
    paddles track mouse and ball bounces off so that user can play,
    sound when ball collides working for user feedback

--- js/game.js
+++ js/game.js
@@ -52,7 +52,7 @@ paintCanvas();
 function trackPosition(evt) {
     mouseObj.x = evt.pageX;
     mouseObj.y = evt.pageY;
-    console.log("cursor x is:" + mouseObj.x + "cursor y is:" + mouseObj.y);
+//    console.log("cursor x is:" + mouseObj.x + "cursor y is:" + mouseObj.y);
 }
 gameCanvas.addEventListener("mousemove", trackPosition, true);
 
@@ -61,7 +61,7 @@ var ball={}; //ball object -- curly brackets
 ball = {
     x: 50,
     y: 50,
-    r: 15,
+    r: 10,
     c: "#ffffff",
     vx: 4, //velocity x
     vy: 8, //velocity y
@@ -108,6 +108,142 @@ function paintScore(){
     
 }
 paintScore();
+
+//Step six...jkh...place paddles top and bottom on canvas
+
+function paddlePosition(TB) {
+    this.w = 150;
+    this.h = 5;
+    
+    this.x = W/2 - this.w/2;
+    
+    if(TB == "top"){
+        this.y = 0;
+    } else {
+        this.y = H - this.h;
+    }
+}
+var paddlesArray = []; //Paddles array
+paddlesArray.push(new paddlePosition("top"));
+paddlesArray.push(new paddlePosition("bottom"));
+//console.log("top paddle y is: " + paddlesArray[0].y);
+//console.log("bottom paddle y is: " + paddlesArray[1].y);
+
+function paintPaddles(){
+    for(var lp = 0; lp < paddlesArray.length; lp++){
+        p = paddlesArray[lp];
+        
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(p.x, p.y, p.w, p.h);
+        
+    }
+}
+paintPaddles();
+
+//step seven..jkh..detect when the user clicks on the screen
+
+gameCanvas.addEventListener("mousedown", btnClick, true);
+
+function btnClick(evt){
+    var mx = evt.pageX;
+    var my = evt.pageY;
+    
+    //user clicked start button
+    if (mx >= startBtn.x && mx <= startBtn.x + startBtn.w){
+       if (my >= startBtn.y && startBtn.y + startBtn.h){
+//           console.log ("start button clicked");
+           //delete the start button
+           startBtn = {};
+           
+           //start game
+           animloop();
+       } 
+    }
+}
+
+//function for running the whole game animation
+var init; //variable to initialize animation
+function animloop(){
+    init = requestAnimFrame(animloop);
+    refreshCanvasFun();
+    
+}
+
+//step eight ..jkh.. draw everything on canvas over and over again
+function refreshCanvasFun(){
+    paintCanvas();
+    paintScore();
+    paintPaddles();
+    ball.draw();
+    update();
+}
+
+function update(){
+    //move the paddles, track the mouse
+    for (var lp = 0; lp < paddlesArray.length; lp++){
+        p = paddlesArray[lp];
+        p.x = mouseObj.x - p.w/2;  
+    }
+    //move the ball
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+    //check for ball paddle collision
+    checkCollision();
+}
+
+function checkCollision(){
+    var pTop = paddlesArray[0];
+    var pBot = paddlesArray[1];
+    
+    if(collides(ball, pTop)){
+        collideAction(ball, pTop);
+    } else if(collides(ball, pBot)) {
+        collideAction(ball, pBot);
+    } else {
+        //ball goes off top or bottom of screen 
+        if(ball.y + ball.r > H){
+            //game over 
+        }else if(ball.y < 0){
+            //game over 
+        }//ball hits side
+            if(ball.x + ball.r > W){
+                ball.vx = -ball.vx;
+                ball.x = W - ball.r;
+            }else if(ball.x - ball.r < 0){
+                ball.vx = -ball.vx;
+                ball.x = ball.r;
+            }      
+        }
+    }
+
+var paddleHit; //which paddle was hit 0=top 1=bottom 
+function collides(b, p){
+    if(b.x + b.r >= p.x && b.x - b.r <= p.x + p.w){
+        if(b.y >= (p.y - p.h)&& p.y > 0){
+            paddleHit = 0;
+            return true;
+        }else if(b.y <= p.h && p.y === 0){
+            paddleHit = 1;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+var collisionSound = document.getElementById("collide");
+function collideAction(b, p){
+//    console.log("sound and then bounce");
+    if (collisionSound){
+    collisionSound.play();
+    }
+    //reverse ball y velocity
+    ball.vy = -ball.vy;
+    //increase score by 1
+    points++;
+}
+    
+
 
 
 
